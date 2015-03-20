bc018d978049e928b6be2d5453f11741258e3e26
virginie <virginietrubiano@gmail.com>
2015-03-20 10:12:36 -0700

    create composer.json and the class Stylist file in src

---
+++ src/Stylist.php
@@ -0,0 +1,102 @@
+<?php 
+
+	class Stylist {
+
+		private name;
+		private id;
+
+		function __construct($name, $id) {
+			$this->name = $name;
+			$this->id = $id;
+		}
+
+		function getName() 
+		{
+			return $this->name; 
+		} 
+
+		function setName($new_name)
+		{
+			return $this->name = (string) $new_name;
+		}
+
+		function getId()
+		{
+			return $this->id;
+		}
+
+		function setId($new_id)
+		{
+			return $this->id = (int) $new_id;
+		}
+
+		function save()
+        {
+            $statement = $GLOBALS['DB']->query("INSERT INTO stylist (name) VALUES ('{$this->getName()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylist;");
+            $stylists = array();
+            foreach($returned_stylists as $stylist) {
+                $name = $stylist['name'];
+                $id = $stylist['id'];
+                $new_stylist = new Stylist($name, $id);
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylist *;");
+        }
+
+        static function find($search_id)
+        {
+            $found_stylist = null;
+            $stylists = Stylist::getAll();
+            foreach($stylists as $stylist) {
+                $stylist_id = $stylist->getId();
+                if ($stylist_id == $search_id) {
+                    $found_stylist = $stylist;
+                }
+              }
+                return $found_stylist;
+        }
+
+//        function getClients()
+//        {
+//            $clients = array();
+//            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE stylist_id = {$this->getId()};");
+//            foreach($returned_clients as $client) {
+//                $name = $client['name'];
+//                $id = $client['id'];
+//                $stylist_id = $client['stylist_id'];
+//                $new_client = new Client($name, $id, $stylist_id);
+//                array_push($clients, $new_client);
+//            }
+//            return $clients;
+//        }
+
+
+        function update($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE stylist SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylist WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM clients WHERE stylist_id = {$this->getId()};");
+        }
+    }
+
+
+	}
+
+?>
