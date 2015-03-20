39cefcb0af1f4e242c760d7772a15edbb695a76b
virginie <virginietrubiano@gmail.com>
2015-03-20 14:15:22 -0700

    add delete all method for the stylist and the add new client to the client_form

--- app/app.php
+++ app/app.php
@@ -18,7 +18,7 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.twig', array('stylists' => Stylist::getAll()));
+        return $app['twig']->render('stylist_form.twig', array('stylists' => Stylist::getAll()));
     });
 
     $app->post('/add_stylist', function() use ($app) {
@@ -28,6 +28,34 @@
     	$new_stylist->save();
     	return $app['twig']->render('stylist_form.twig', array('stylists' => Stylist::getAll()));
     });
+
+	$app->get('/stylist_form/{id}', function($id) use ($app) {
+		$stylist = Stylist::find($id);
+		return $app['twig']->render('client_form.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+	});
+
+	$app->post('/delete_all_the_stylists', function() use ($app) {
+		Stylist::deleteAll();
+		return $app['twig']->render('stylist_form.twig', array('stylists' => Stylist::getAll()));
+	});
+
+	$app->post('/add_client', function() use ($app) {
+		$name = $_POST['name'];
+		$id = null;
+		$stylist_id = $_POST['stylist_id'];
+		$new_client = new Client($name, $id, $stylist_id);
+		$new_client->save();
+		$stylist = Stylist::find($stylist_id);
+		return $app['twig']->render('client_form.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+	});
+
+	$app->post('/delete_all_the_clients', function() use ($app) {
+		$stylist_id = $_POST['stylist_id'];
+		$stylist = Stylist::find($stylist_id);
+		Client::deleteAll();
+
+		return $app['twig']->render('client_form.twig', array('stylist' => $stylist, 'clients' => Client::getAll()));
+	});
 
     return $app;
 
